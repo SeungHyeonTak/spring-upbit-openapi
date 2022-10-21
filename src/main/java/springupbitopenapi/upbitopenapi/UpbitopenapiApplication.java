4a08add1f307101ed160efd375a817cda6e2712e
SeungHyeonTak-sub <conficker77@gmail.com>
2022-10-21 14:42:20 +0900

    add, first commit

---
+++ src/main/java/springupbitopenapi/upbitopenapi/UpbitopenapiApplication.java
@@ -0,0 +1,13 @@
+package springupbitopenapi.upbitopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitopenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpbitopenapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springupbitopenapi/upbitopenapi/UpbitopenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package springupbitopenapi.upbitopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbitopenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
